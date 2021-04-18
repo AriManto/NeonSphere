9e1f527b2eec94677505efbcaaef59865036bb9b
AriManto <37150326+AriManto@users.noreply.github.com>
2021-04-18 03:33:46 -0300

    Restart UI, death

---
+++ Assets/NeonSphere - Starter Pack/Scripts/BlockTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTrigger : MonoBehaviour
+{
+    [SerializeField] private HealthController myHealthController;
+    [SerializeField] private int damage;
+    [SerializeField] private int heal;
+
+
+    [SerializeField] private bool damageBool;
+    [SerializeField] private bool healBool;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (damageBool)
+            {
+                myHealthController.playerHealth -= damage;
+                myHealthController.UpdateHealth();
+            } else if (healBool)
+            {
+                myHealthController.playerHealth += heal;
+                myHealthController.UpdateHealth();
+            }
+        }
+    }
+
+}
---
+++ Assets/NeonSphere - Starter Pack/Scripts/TriggerExamples.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.NeonSphere___Starter_Pack.Scripts
+{
+    public class TriggerExamples : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("I have entered the collider!");
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("I have exited the collider!");
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("I am inside the collider!");
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
